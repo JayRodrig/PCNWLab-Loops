6d62d765f8673b447a87268c586e800ac998ef49
J a y <joserodriguez@Js-MacBook-Pro.local>
2018-10-20 17:05:09 -0400

    added answer to p1 pset2.js

--- pset2.js
+++ pset2.js
@@ -6,6 +6,17 @@
     @example - removeNegatives([-3,-2,1,2,3,-4]); // [3,2,1,2,3,4]
 */
 
+const removeNegatives = arr => {
+    const copyArr = [];
+    for(i = 0; i < arr.length; i++){
+        copyArr.push(Math.abs(arr[i]));
+    }
+    return copyArr;
+}
+
+console.log(removeNegatives([-3,-2,1,2,3,-4]));
+console.log(removeNegatives([-10, -8, -9, -15]));
+console.log('*********************************************************************')
 /*
     @func findVal
     @param {array} arr
